c3c936a49b7a8147f8b312d63d419f94a5da851b
Justin Senteu <sjsenteu@gmail.com>
2019-05-30 17:01:23 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<center>
+		<b>
+	<form action="screenscaster-3b.php" method="post">
+	Name: <input type="text" name="$fperson"/> <br>
+	Name: <input type="text" name="$sperson"/> <br>
+	Name: <input type="text" name="$tperson"/>  <br>
+	Name: <input type="text" name="$xperson"/>  <br>    
+<input type="submit" value="search"/>  
+</b>
+</center>
+	</form>
+
+</body>
+</html>
---
+++ screenscaster-3b.php
@@ -0,0 +1,21 @@
+ <?php 
+
+
+		// PHP program to create  
+// multidimensional array 
+  
+// Creating multidimensional 
+// array 
+$myarray = array( 
+      
+    // Default key for each will 
+    // start from 0 
+    array('$fperson','$sperson', '$tperson', '$xperson'), 
+    array('$fnumber', '$snumber', '$tnumber','$xnumber'),
+    array('$fsum', '$ssum', '$tsum','$xsum') 
+
+); 
+      
+// Display the array information 
+print_r($myarray); 
+}
